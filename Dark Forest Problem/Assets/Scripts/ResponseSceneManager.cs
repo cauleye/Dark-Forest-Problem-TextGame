95c218e84d4092f3b01ea81469a4e7143b91206e
elijahcauley <elijah.cauley@lawrence.edu>
2016-08-14 23:40:06 -0400

    Added the whole project. Be ware the the entirety of Ostrich Sans is in the project, so it has a lot of files.

---
+++ Dark Forest Problem/Assets/Scripts/ResponseSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResponseSceneManager : MonoBehaviour {
+
+	public GameObject peaceButton;
+	public GameObject peaceText;
+	public GameObject deathText;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Sets all game objects inactive
+		peaceButton.SetActive (false);
+		peaceText.SetActive (false);
+		deathText.SetActive (false);
+
+		int randomNumber = Random.Range (1, 3); //Between 1 and 3 b/c 3 is exclusive but 1 is inclusive
+		Debug.Log ("The random number is:" + randomNumber);
+
+		if (randomNumber == 1) {
+			// This code is to run the peaceful scenario
+			peaceButton.SetActive (true);
+			peaceText.SetActive (true);
+		}
+
+		if (randomNumber == 2) {
+			// This code is to run the death scenario
+			deathText.SetActive (true);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
